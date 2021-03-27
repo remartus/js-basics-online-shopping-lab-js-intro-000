67409f121d209b7b9c5a3b2ae4770b758de61bdd
Rose Martus <martus.rose@gmail.com>
2021-03-27 21:58:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function viewCart() {
   }
 
   if (cart.length === 1){
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0]}`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }
   else{
     var response = "In your cart, you have "
