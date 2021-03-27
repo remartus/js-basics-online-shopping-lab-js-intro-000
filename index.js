d1279b58040058fbc717a26ac13e16a79f515550
Rose Martus <martus.rose@gmail.com>
2021-03-27 22:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  var present = yes
   for(let i = 0; i < cart.length; i++){
     if(cart[i].itemName === item){
       cart.splice(i,1)
