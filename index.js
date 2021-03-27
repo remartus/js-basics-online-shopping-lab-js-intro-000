a54123df37271243eac1d35f063f28721533eef0
Rose Martus <martus.rose@gmail.com>
2021-03-27 21:46:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  }
 }
 
 function total() {
