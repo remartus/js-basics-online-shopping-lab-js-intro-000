1a840eff23f0e76fb9747c271d2f9c032a782a8a
Rose Martus <martus.rose@gmail.com>
2021-03-27 22:27:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined){
-    return "Sorry, we don't have a credit card on file for you"
+    return "Sorry, we don't have a credit card on file for you."
   }
 }
