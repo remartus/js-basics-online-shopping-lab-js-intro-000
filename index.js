c53079318f07c9fa3b67f6ea14cc89519d9006ba
Rose Martus <martus.rose@gmail.com>
2021-03-27 22:08:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function viewCart() {
     for(let i = 0; i < cart.length - 1; i++){
       response = `${response}${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
-    response = `${response} and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
+    response = `${response}and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
     return response
   }
 }
