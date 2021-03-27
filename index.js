4123a81202603616ae59ad63bc50a70fa682fda0
Rose Martus <martus.rose@gmail.com>
2021-03-27 22:29:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
-    var total = total()
-    return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`
+    var totalPrice = total()
+    return `Your total cost is ${totalPrice}, which will be charged to the card ${cardNumber}.`
   }
 
 }
